3aef93c5d428995b93438f30c0a5efbcd9280294
Brou Kouadio Stéphane Fabien <fabienbrou99@gmail.com>
2023-06-20 14:20:54 +0000

    ajout de la base de donnée

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from Class.aami import AsteriskAmi
 
 app = Flask(__name__)
 
-
 # DEFINITION DES ROUTES
 @app.post("/")
 def make_call_from_whatsapp():
@@ -13,14 +12,20 @@ def make_call_from_whatsapp():
     if phones:
         ami = AsteriskAmi(phones["chatId"], phones["caller"], phones["receiver"])
         data = ami.make_external_call()
-        print("data : ", data)
     return "make call"
 
 @app.get("/internal")
 def make_an_internal_call():
     ami = AsteriskAmi("", "701", "702")
     data = ami.make_internal_call()
-    print("data : ", data)
+    #atempt = Attempts()
+    #atempt.add("701", "702", "e")
+    """
+    'hangup': {'Event': 'Hangup', 'Privilege': 'call,all', 'Channel': 'PJSIP/701-0000005d', 'ChannelState': '5', 'ChannelStateDesc': 'Ringing', 'CallerIDNum': '701', 'CallerIDName': 'caller', 'ConnectedLineNum': '<unknown>', 'ConnectedLineName': '<unknown>', 'Language': 'en', 'AccountCode': '', 'Context': 'from-internal', 'Exten': 's', 'Priority': '1', 'Uniqueid': '1687265672.140', 'Linkedid': '1687265672.140', 'Cause': '17', 'Cause-txt': 'User busy'}
+    """
+    
+   # atempt.add("701", "702", data["hangup"]['ChannelStateDesc'])
     return "e"
+
 if (__name__) == "__main__":
     app.run()
